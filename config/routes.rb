d09fa646b7a451b98a0b3aa080a94aaa1d96a3f4
ZengTao <so.zengtao@gmail.com>
2016-08-19 13:47:26 +0800

    注册时候带上用户名

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :events, only: [:index]
 
   root 'events#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
